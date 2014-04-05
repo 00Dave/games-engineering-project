be5389df75d76348ba73b4b12d68aa7b63682518
Dave Russell <dave.allan.russell@gmail.com>
2014-04-05 01:59:06 +0100

    clearing my branch

--- ThePlayer/StateNormal.cpp
+++
@@ -1,8 +0,0 @@
-#include "StateNormal.h"
-
-using namespace GameEngine;
-
-void StateNormal::execute(Enemy* owner, float deltaTime){
-	//set the angular velocity of the enemy to 0
-	owner->getRigidBody()->setLinearVelocity(btVector3(0,0,0));
-}
--- ThePlayer/StateNormal.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Enemy.h"
-
-using namespace GameEngine;
-
-class StateNormal : public State<Enemy>{
-public:
-	void execute(Enemy* owner, float deltaTime);
-};
